629b743975ca4577c6bc6833c823ca18ae8595d2
ZornTaov <zeke_kjell2@yahoo.com>
2019-12-04 02:05:49 -0800

    tweak names

--- app/src/main/java/org/zornco/ra_playlist_maker/common/BackStackManager.kt
+++ app/src/main/java/org/zornco/ra_playlist_maker/common/BackStackManager.kt
@@ -3,29 +3,29 @@ package org.zornco.ra_playlist_maker.common
 
 class BackStackManager<T>
 {
-    private var files = mutableListOf<T>()
+    private var stack = mutableListOf<T>()
     var onStackChangeListener: ((List<T>) -> Unit)? = null
 
     val top:T
-        get() = files[files.size - 1]
+        get() = stack[stack.size - 1]
 
-    fun addToStack(fileModel: T)
+    fun addToStack(t: T)
     {
-        files.add(fileModel)
-        onStackChangeListener?.invoke(files)
+        stack.add(t)
+        onStackChangeListener?.invoke(stack)
     }
 
     fun popFromStack()
     {
-        if (files.isNotEmpty())
+        if (stack.isNotEmpty())
         {
-            files.removeAt(files.size - 1)
+            stack.removeAt(stack.size - 1)
         }
-        onStackChangeListener?.invoke(files)
+        onStackChangeListener?.invoke(stack)
     }
-    fun popFromStackTill(fileModel: T)
+    fun popFromStackTill(t: T)
     {
-        files = files.subList(0,files.indexOf(fileModel)+1)
-        onStackChangeListener?.invoke(files)
+        stack = stack.subList(0,stack.indexOf(t)+1)
+        onStackChangeListener?.invoke(stack)
     }
 }

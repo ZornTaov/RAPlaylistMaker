8fa7e1f0a6cc9596c5608417eb42f16403c60695
ZornTaov <zeke_kjell2@yahoo.com>
2019-12-10 22:55:09 -0800

    add Entry Editor Activity

---
+++ app/src/main/java/org/zornco/ra_playlist_maker/playlist/EntryEditorActivity.kt
@@ -0,0 +1,13 @@
+package org.zornco.ra_playlist_maker.playlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.zornco.ra_playlist_maker.R
+
+class EntryEditorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entry_editor)
+    }
+}
